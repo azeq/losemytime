00c2c4ac59b35d8bf8297236bec1f7ecb961907b
azeq <bares.paul@gmail.com>
2013-11-17 19:21:45 +0100

    votes improvements

--- database.py
+++ database.py
@@ -3,41 +3,63 @@
 
 import sqlite3 as lite
 
-def insertNewVote(videoId, getBored, userId):
-    con = lite.connect('lyt.db')
+nameOfDatabase = u"videovote"
 
+def insertNewVote(videoId, getBored, userId):
     print "insertNewVote"
     print "videoId: "+videoId
     print "getBored: "+getBored
     print "userId: "+str(userId)
 
+    valid = False
+    con = lite.connect('lyt.db')
     with con:
         cur = con.cursor()    
         
         # test if the entry exists
-        cur.execute("select exists(select * from videovote where video_id=? and user_id=?)", (videoId, userId))
+        cur.execute("select exists(select * from "+nameOfDatabase+" where video_id=? and user_id=?)", (videoId, userId))
         rows = cur.fetchall() # its a list
 
         if len(rows) == 1:
             row = rows[0] # row is a tuple
-
             # if the entry exisits, update it        
             # update get bored for a video
             if row[0] == 1:
                 print "update database"
-                cur.execute("update videovote set get_bored=? where video_id=? and user_id=?", (getBored, videoId, userId))
+                cur.execute("update "+nameOfDatabase+" set get_bored=? where video_id=? and user_id=?", (getBored, videoId, userId))
+                valid = True
             # new entry
             elif row[0] == 0:
                 print "add new entry" 
-                cur.execute("insert into videovote(video_id, get_bored, user_id) values('"+videoId+"', '"+getBored+"', '"+userId+"')")
+                cur.execute("insert into "+nameOfDatabase+"(video_id, get_bored, user_id) values('"+videoId+"', '"+getBored+"', '"+userId+"')")
+                valid = True
             else:
-                print "error"       
+                valid = False
+                print "error"   
+    return valid    
 
     # lid = cur.lastrowid
     # print "The last Id of the inserted row is %s" % lid
 
-        cur.execute("SELECT * FROM videovote")
-        rows = cur.fetchall() # its a list
+    # cur.execute("SELECT * FROM "+nameOfDatabase)
+    # rows = cur.fetchall() # its a list
 
-        for row in rows:
-            print row
+    # for row in rows:
+    #     print row
+
+def computeAllVotes(videoId = None):
+    if videoId != None:
+        con = lite.connect('lyt.db')
+        with con:
+            cur = con.cursor()
+            cur.execute("select * from "+nameOfDatabase+" where video_id=? and get_bored=?", (videoId, 'True'))
+            rowsBored = cur.fetchall()
+
+            cur.execute("select * from "+nameOfDatabase+" where video_id=? and get_bored=?", (videoId, 'False'))
+            rowsLiked = cur.fetchall()
+
+            return [len(rowsBored), len(rowsLiked)]
+            
+            # print len(rowsBored)
+            # print len(rowsLiked)
+
--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import cherrypy
 from jinja2 import Environment, FileSystemLoader
 from ytsearch import RandomVideoBuilder
 from vote import insertNewElement
+from database import computeAllVotes
 from database import insertNewVote
 
+host = 'localhost'
 port = 8000
-cherrypy.config.update({'server.socket_host': '127.0.0.1',
+url = host+':'+str(port)
+
+cherrypy.config.update({'server.socket_host': host,
                         'server.socket_port': port,
                        })
 
@@ -23,33 +27,48 @@ class Root:
     def __init__(self):
         self.userID = None
         self.stat = None
+        self.paricularVideoId = None
 
     @cherrypy.expose
-    def index(self):
+    def index(self, videoId = None):
         tmpl = env.get_template('index.html')
         rvb.recompute()
         rvb.debug() # print things
-        return tmpl.render(title='Lose my time', currentVideoId=rvb.getCurrentVideoId(), nextVideoId=rvb.getNextVideoId(),
-        	videoTitle=rvb.getVideoTitle(), wikilink=wikipediaPythonLink, url='/')
+        if videoId == None:
+            videoId = rvb.getCurrentVideoId()
+        self.setVideoId(videoId) # load in the page a particularVideo, http://localhost:8000/?videoId=f9O5F1eiIjI
+        votes = computeAllVotes(videoId)
+        return tmpl.render(title='Lose my time', currentVideoId=self.paricularVideoId, nextVideoId=rvb.getNextVideoId(),
+        	videoTitle=rvb.getVideoTitle(), wikilink=wikipediaPythonLink, path='./', nbBored = votes[0], nbLiked = votes[1])
 
     @cherrypy.expose
     def concept(self):
         t = env.get_template('concept.html')
         print self.userID
-        return t.render(title2 = 'Concept', pathStyle='../', url='../', userID = self.userID)   
+        return t.render(title2 = 'Concept', path='../', userID = self.userID)   
 
     @cherrypy.expose
     def doVote(self, videoId = None, hasGotBored = None):
-        print "===>>>" + str(videoId) + " - " + str(hasGotBored)
-        insertNewElement([str(videoId),str(hasGotBored)])
-        if self.userID != None and videoId != None and hasGotBored != None:
-            insertNewVote(str(videoId), hasGotBored, self.userID)
-    
+        # print "===>>>" + str(videoId) + " - " + str(hasGotBored)
+        # insertNewElement([str(videoId),str(hasGotBored)])
+        if self.userID != None and videoId != None and hasGotBored != None and self.stat == 'connected':
+            valid = insertNewVote(str(videoId), hasGotBored, self.userID) # return true if the insertion is valid (ie an update or an new entry)
+            if valid:
+                self.refreshVotes(videoId)
+
     @cherrypy.expose
     def updateFbInfo(self, userID = None, token = None, stat = None):
         print "update user info"
         self.userID = userID
         self.stat = stat
+
+    def refreshVotes(self, videoId):
+        computeAllVotes(videoId) # need a refresh dynamically
+        self.index(videoId) # this does not refresh the page, does nothing....
+
+    # setter
+    def setVideoId(self, videoId):
+        self.paricularVideoId = videoId
 
 root = Root()
 cherrypy.quickstart(root, '/', 'main.config')
